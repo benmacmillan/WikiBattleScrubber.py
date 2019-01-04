19935aaaad6ac7417cbb37e88198b4f36efa6fb2
Ben <45116438+benmacmillan@users.noreply.github.com>
2019-01-04 10:51:33 +0800

    Update cl_scraper.py
    adds tabulate to the project to display information neatly/future output changes

--- cl_scraper.py
+++ cl_scraper.py
@@ -1,28 +1,30 @@
 #Take a wikipedia URL for a battle and then output a file with the name, date and results of the battle.
 import requests
+from tabulate import tabulate
 from bs4 import BeautifulSoup
 from argparse import ArgumentParser
 #----------------------------------------------------------------------------
-#Enable parsing through command line, prepare the soup. 
+#Parsing
 parser = ArgumentParser(description='Pull battle details from wikipedia')
 parser.add_argument('-url', type=str, help='Usage: -url followed by the wiki URL you wish to use')
 args = vars(parser.parse_args())
+
+#Soup
 url = args['url']
 battle = requests.get(url).text
 soup = BeautifulSoup(battle,'lxml')
 
-#Pull information from the battles side bar:
+#Information to scrape
 Name = soup.find("h1", {"class":"firstHeading"})
 Date = soup.find(text="Date").findNext('td')
 Location = soup.find(text="Location").findNext('td')
 Result = soup.find(text="Result").findNext('td')
+Table = [["Name",Name.text],["Date",Date.text],["Result",Result.text]]
 FileName = (Name.text)
 
-#Show to user and save in a text file:
-print (Name.text)
-print (Date.text)
-print (Result.text)
+#Display in console
+print (tabulate(Table))
 
-#write the details to a text file
+#Write to file 
 outfile = open(FileName + ".txt","w")
-outfile.write(Name.text + '\n' + Date.text + '\n' + Result.text)
+outfile.write(tabulate(Table))
