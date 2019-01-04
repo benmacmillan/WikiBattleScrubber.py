1a46fed347e1fbafc0ff204caa266b910a0a475d
Ben <45116438+benmacmillan@users.noreply.github.com>
2019-01-04 11:41:20 +0800

    Update WikiBattleScraper.py
    improved script styling

--- WikiBattleScraper.py
+++ WikiBattleScraper.py
@@ -1,24 +1,25 @@
-#Take a wikipedia URL for a battle and then output a file with the name, date and results of the battle.
-
+# Wiki Battle Scraper - Script
 import requests
 from tabulate import tabulate
 from bs4 import BeautifulSoup
-#----------------------------------------------------------------------------
-#Prepare that tasty and beautiful soup to read the web page:
-battles_url = requests.get('https://en.wikipedia.org/wiki/Battle_of_Waterloo').text
-soup = BeautifulSoup(battles_url,'lxml')
+# ----------------------------------------------------------------------------
+# Prepare a tasty and beautiful soup to scrape:
+battle = requests.get('https://en.wikipedia.org/wiki/Battle_of_Waterloo').text
+soup = BeautifulSoup(battle, 'lxml')
 
-#Pull information from the battles side bar:
-Name = soup.find("h1", {"class":"firstHeading"})
-Date = soup.find(text="Date").findNext('td')
-Location = soup.find(text="Location").findNext('td')
-Result = soup.find(text="Result").findNext('td')
-Table = [["Name",Name.text],["Date",Date.text],["Result",Result.text]]
-FileName = (Name.text)
+# Pull information from side bar:
+name = soup.find("h1", {"class": "firstHeading"})
+date = soup.find(text="Date").findNext('td')
+location = soup.find(text="Location").findNext('td')
+result = soup.find(text="Result").findNext('td')
 
-#Show to user in console
-print (tabulate(Table))
+#Put information into a table, set the output file name.
+table = [["Name", name.text], ["Date", date.text], ["Result", result.text]]
+fileName = (name.text)
 
-#write the details to a text file
-outfile = open(FileName + ".txt","w")
-outfile.write(tabulate(Table))
+# Show to user in console
+print (tabulate(table))
+
+# write details to file
+outfile = open(fileName + ".txt", "w")
+outfile.write(tabulate(table))
